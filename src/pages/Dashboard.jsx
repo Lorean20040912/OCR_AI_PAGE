44de151c395830f805a9886bd2f5ec6df1c8d2d7
lorea <loreanmandrescu2004@gmail.com>
2026-05-08 22:43:01 +0300

    am conectat dashboard stats cu localstorage

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,13 +1,21 @@
 import StatsCard from "../components/StatsCard"
+import { useState, useEffect } from 'react'
+// State tine minte initial, effect daca observa schimabare modifica
+import { getFacturi } from '../utils/storage'
+// .. da inapoi in mapa parinte
 
 function Dashboard() {
+    const [facturi, setFacturi] = useState([])
+    useEffect(() => {
+        setFacturi(getFacturi())
+    }, [])
     return (
      <div>
        <h1>Dashboard</h1>
-       <StatsCard numar={0} titlu="Total Facturi"/>
-       <StatsCard numar={1} titlu="Aprobate"/>
-       <StatsCard numar={2} titlu="Respinse"/>
-       <StatsCard numar={3} titlu="In asteptare"/>
+       <StatsCard numar={facturi.length} titlu="Total Facturi"/>
+       <StatsCard numar={facturi.filter(f => f.status === 'aprobata').length} titlu="Aprobate"/>
+       <StatsCard numar={facturi.filter(f => f.status === 'respinsa').length} titlu="Respinse"/>
+       <StatsCard numar={facturi.filter(f => f.status === 'in_asteptare').length} titlu="In asteptare"/>
      </div>
     )
 }
